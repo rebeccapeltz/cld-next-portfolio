d1d3c6ddf0378e6d5bb67ea5353ef2da6d0fea8c
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-12-29 11:38:04 -0800

    working on vp;change og image

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,6 @@
 import Container from './container'
 import { EXAMPLE_PATH } from '../lib/constants'
+import VideoPlayer from '../components/video-player'
 
 export default function Footer() {
   return (
@@ -9,6 +10,7 @@ export default function Footer() {
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Statically Generated with Next.js.
           </h3>
+          {/* <VideoPlayer /> */}
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://nextjs.org/docs/basic-features/pages"
