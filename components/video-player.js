34b24134948027cfc6b12e1465ba6c3c2a0cb751
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-12-29 13:23:37 -0800

    add video player

--- components/video-player.js
+++ components/video-player.js
@@ -1,34 +1,25 @@
-import dynamic from "next/dynamic";
-
-const DynamicComponent = dynamic(() =>
-  import("cloudinary-core").then((Cloudinary) => {
-    import("cloudinary-video-player/dist/cld-video-player").then((vplib,Cloudinary) => {
-      const cloudinary = new Cloudinary({
-        cloud_name: "demo",
-        secure: true,
-      });
-      const videoPlayerInit = () => {
-        const cld = cloudinary();
-        cld.videoPlayer("vp", {
-          publicId: "race_road_car",
-          controls: true,
-          preload: "auto",
-          muted: true,
-          autoplay: false,
-          width: 300,
-        });
-      };
-      useEffect(() => {
-        videoPlayerInit();
-      }, []);
-    });
-  })
-);
+import React, { useEffect } from 'react';
 
 export default function VideoPlayer() {
+  useEffect(() => {
+    const cld = new cloudinary.Cloudinary({
+      cloud_name: "demo",
+      secure: true,
+    });
+    const videoPlayerInit = () => {
+      cld.videoPlayer("vp", {
+        publicId: "race_road_car",
+        controls: true,
+        preload: "auto",
+        muted: true,
+        autoplay: false,
+        width: 300,
+      });
+    };
+    videoPlayerInit();
+  }, []);
   return (
     <div>
-      <DynamicComponent />
       <video id="vp" />
     </div>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,28 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://unpkg.com/cloudinary-video-player@1.5.1/dist/cld-video-player.min.css"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
+          <script
+            src="https://unpkg.com/cloudinary-core@latest/cloudinary-core-shrinkwrap.min.js"
+            type="text/javascript"
+          ></script>
+          <script
+            src="https://unpkg.com/cloudinary-video-player@1.5.1/dist/cld-video-player.min.js"
+            type="text/javascript"
+          ></script>
         </body>
       </Html>
-    )
+    );
   }
 }
