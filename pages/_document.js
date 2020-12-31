9e1a953d4b57dc5880b29fe505fa01d84caaa302
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-12-31 09:23:14 -0800

    add lodash

--- pages/_document.js
+++ pages/_document.js
@@ -13,11 +13,15 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script
+          <script defer
+            src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js"
+            type="text/javascript"
+          ></script>
+          <script defer
             src="https://unpkg.com/cloudinary-core/cloudinary-core-shrinkwrap.min.js"
             type="text/javascript"
           ></script>
-          <script
+          <script defer
             src="https://unpkg.com/cloudinary-video-player/dist/cld-video-player.min.js"
             type="text/javascript"
           ></script>
